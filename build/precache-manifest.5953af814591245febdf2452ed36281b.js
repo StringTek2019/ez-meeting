8cc01f407fc97f5fa707309898c543171442e2e7
StringTek <stringtek1998@outlook.com>
2019-12-18 21:18:25 +0800

    init build

---
+++ build/precache-manifest.5953af814591245febdf2452ed36281b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964e6b5ab5f4fcfe6bc775737f8459e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ee2ca9fc9b382f9633a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "05d066b2cde65c4f2e79",
+    "url": "/static/js/2.b966a718.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b966a718.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ee2ca9fc9b382f9633a",
+    "url": "/static/js/main.b66799d6.chunk.js"
+  },
+  {
+    "revision": "32b425b8617965f52506",
+    "url": "/static/js/runtime-main.f35929a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
